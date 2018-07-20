a795facc6c5db7cd9dad4d982c94d7bd96665230
admin <admin@example.com>
2018-07-20 15:59:00 +0800

    update

--- files/redis_init_system_conf.sh
+++ files/redis_init_system_conf.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
-sysctl vm.overcommit_memory=1
-echo "echo never > /sys/kernel/mm/transparent_hugepage/enabled" >> /etc/rc.local
+sysctl -w vm.overcommit_memory=1
+sysctl -w vm.swappiness=0
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never > /sys/kernel/mm/transparent_hugepage/defrag
+
+echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
+echo "vm.swappiness = 0" >> /etc/sysctl.conf
+sed -i "20 i\echo never > /sys/kernel/mm/transparent_hugepage/enabled" /etc/rc.local
+sed -i "20 i\echo never > /sys/kernel/mm/transparent_hugepage/defrag" /etc/rc.local
